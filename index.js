c99f4d551d9cd3d280ba9c3a3e55d1ed86f76363
Aristela Marku <aristela.marku@gmail.com>
2021-10-07 22:21:46 +0200

    commit tree 6e8de170bbbef87c6f9b2f139aafce4c56443bdf

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase()
+  }
+  static sanitize(string){
+    return string.replace(/[^\w\s]/gi, '')
+  }
 }
+
+Formatter.capitalize("alligator")
